81c99fce86253627c5e006c7fa6bb4e9f519a23d
bhawnapandit26 <bhawnapandit26@gmail.com>
2026-02-11 00:25:53 +0530

    details updated

--- src/Component/Chart.jsx
+++ src/Component/Chart.jsx
@@ -8,6 +8,10 @@ import {
 } from "recharts";
 
 export default function MonthlyChart({ data, selectedMonths }) {
+
+    const COLORS = ["#2563eb", "#16a34a", "#dc2626", "#9333ea", "#f59e0b"];
+
+
   return (
     <div className="bg-white p-4 rounded shadow h-72">
       <ResponsiveContainer width="100%" height="100%">
@@ -16,12 +20,13 @@ export default function MonthlyChart({ data, selectedMonths }) {
           <YAxis />
           <Tooltip />
 
-          {selectedMonths.map(month => (
+          {selectedMonths.map((month, index) => (
             <Line
               key={month}
               type="monotone"
               dataKey={month}
               strokeWidth={2}
+              stroke={COLORS[index % COLORS.length]}
             />
           ))}
 
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
     return(
         <div className="w-full shadow-md shadow-gray-200 py-4 px-4 border border-gray-200 rounded-4xl flex justify-between">
-            <div>
-                <img src="https://www.upskillist.com/images/logo/header-logo.svg" alt="upskillist logo" className="w-44"/>
+            <div className="font-bold text-xl text-gray-900">
+                COMPANY NAME
             </div>
             <div className="font-bold text-xl text-gray-900">
                 Monthly Marketing Report
